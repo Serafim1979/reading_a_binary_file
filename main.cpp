6ea4d7e4cae2672b1fac3bcf2e21cff7232a54aa
Ivan Korolkov <korolkov.ivan@mail.ru>
2023-01-02 20:29:12 +0300

    added feature

--- main.cpp
+++ main.cpp
@@ -9,15 +9,15 @@ struct DATA
 	short int month[12];
 }
 
-void read_binary_file(std::vector<STRUCT_NAME> &vec, const std::string &file_path)
+void read_binary_file(std::vector<DATA> &vec, const std::string &file_path)
 {
 	std::ifstream ifs(file_path, std::ifstream::binary);
 	assert(ifs.is_open());
-	STRUCT_NAME st;
+	DATA st;
 
 	while (!ifs.eof())
 	{
-		ifs.read(reinterpret_cast<char*>(&st), sizeof(STRUCT_NAME));
+		ifs.read(reinterpret_cast<char*>(&st), sizeof(DATA));
 		vec.push_back(st);
 	}
 
